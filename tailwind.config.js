5e53d19946f20e9377529e9c49e0a18dfd2ba0f3
Manas Gupta <guptamanas1998@gmail.com>
2021-06-20 17:57:03 +0530

    Fix dark mode color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-import { defaultTheme } from 'tailwindcss/defaultTheme'
-
 module.exports = {
   theme: {
     colors: {
         white: '#ffffff',
-        black: '#23272a'
+        black: '#2f3437'
       }
   }
 }
